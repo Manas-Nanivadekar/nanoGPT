5c50ef708334bfb2d60feedb8dbc455a59064c87
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-06-20 11:41:41 +0530

    added single head attention

--- v2.py
+++ v2.py
@@ -5,9 +5,9 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32
 block_size = 8
-max_iters = 3000
-eval_intervals = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_intervals = 500
+learning_rate = 1e-3
 device = "cuda"
 eval_iters = 200
 n_embd = 32
@@ -59,16 +59,49 @@ def estimate_loss():
     return out
 
 
+class Head(nn.Module):
+    """one head of self attention"""
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        # computer attention scores ('affinities')
+        wei = (
+            q @ k.transpose(-2, -1) * C**-0.5
+        )  # (B, T, 16) @ (B, 16, T) ---> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)
+        # perform the weighted aggregation of the values
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+
 class BigramLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
+        B, T = idx.shape
+
         token_embeddings = self.token_embedding_table(idx)  # (B, T, C)
-        logits = self.lm_head(token_embeddings)  # (B, T, vocab_size)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+        x = token_embeddings + pos_emb  # (B, T, C)
+        x = self.sa_head(x)
+        logits = self.lm_head(x)  # (B, T, vocab_size)
 
         if targets == None:
             loss = None
@@ -82,7 +115,8 @@ class BigramLanguageModel(nn.Module):
 
     def generate(self, idx, max_new_tokens):
         for _ in range(max_new_tokens):
-            logits, loss = self(idx)
+            idx_cond = idx[:, -block_size:]
+            logits, loss = self(idx_cond)
             logits = logits[:, -1, :]
             probs = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
